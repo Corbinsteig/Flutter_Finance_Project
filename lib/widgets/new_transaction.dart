32947218b54e88f79713049eddc232ef5f824cb9
Corbin Steig <corsteig1@gmail.com>
2022-11-03 11:47:13 -0700

    Added final files

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
+
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
+    );
+
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+    print('...');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,30 +63,55 @@ class NewTransaction extends StatelessWidget {
           children: <Widget>[
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-              // onChanged: (value) {
-              //   titleInput = value;
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
+              // onChanged: (val) {
+              //   titleInput = val;
               // },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
-              // onChanged: (value) {
-              //   amountInput = value;
-              // }
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitData(),
+              // onChanged: (val) => amountInput = val,
             ),
-            TextButton(
-              child: Text('Add Transaction'),
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all(Colors.purple),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
+                    ),
+                  ),
+                  TextButton(
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          Theme.of(context).primaryColor),
+                    ),
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: _presentDatePicker,
+                  ),
+                ],
               ),
-              onPressed: () {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-                // print(amountController.text);
-              },
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).primaryColor),
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).primaryColor),
+              ),
+              child: Text('Add Transaction'),
+              onPressed: _submitData,
             ),
           ],
         ),
--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -1,50 +1,52 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import './new_transaction.dart';
-import './transaction_list.dart';
-import '../models/transaction.dart';
+// import './new_transaction.dart';
+// import './transaction_list.dart';
+// import '../models/transaction.dart';
+// import './user_transactions.dart';
 
-class UserTransactions extends StatefulWidget {
-  @override
-  _UserTransactionsState createState() => _UserTransactionsState();
-}
 
-class _UserTransactionsState extends State<UserTransactions> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
+// class UserTransactions extends StatefulWidget {
+//   @override
+//   _UserTransactionsState createState() => _UserTransactionsState();
+// }
 
-  void _addNewTransaction(String txTitle, double txAmount) {
-    final newTx = Transaction(
-      title: txTitle,
-      amount: txAmount,
-      date: DateTime.now(),
-      id: DateTime.now().toString(),
-    );
+// class _UserTransactionsState extends State<UserTransactions> {
+//   final List<Transaction> _userTransactions = [
+//     Transaction(
+//       id: 't1',
+//       title: 'New Shoes',
+//       amount: 69.99,
+//       date: DateTime.now(),
+//     ),
+//     Transaction(
+//       id: 't2',
+//       title: 'Weekly Groceries',
+//       amount: 16.53,
+//       date: DateTime.now(),
+//     ),
+//   ];
 
-    setState(() {
-      _userTransactions.add(newTx);
-    });
-  }
+//   void _addNewTransaction(String txTitle, double txAmount) {
+//     final newTx = Transaction(
+//       title: txTitle,
+//       amount: txAmount,
+//       date: DateTime.now(),
+//       id: DateTime.now().toString(),
+//     );
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        NewTransaction(_addNewTransaction),
-        TransactionList(_userTransactions),
-      ],
-    );
-  }
-}
+//     setState(() {
+//       _userTransactions.add(newTx);
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         NewTransaction(_addNewTransaction),
+//         TransactionList(_userTransactions),
+//       ],
+//     );
+//   }
+// }
